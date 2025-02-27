1cb4ec5615a9e169869ab49a18fcd3c1846f9bd3
Alexandr <alexandrvjosan@gmail.com>
2025-02-27 10:18:46 +0200

    feat(index): add swagger UI

--- hello/urls.py
+++ hello/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+
 from . import views
 
 urlpatterns = [
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('api/index/', views.index),
 ]
